3ff3e57a39219249b395ba4125090b3971f5ac74
dheim123 <dheim@midland.k12.ia.us>
2026-03-25 10:43:28 -0500

    Implement CreateTeam method to return team list

--- Classroom.cs
+++ Classroom.cs
@@ -1,8 +1,8 @@
 ﻿namespace knightmoves;
 
-public class Classroom{
+public class Classroom {
 
-    public List<string> CreateTeam(string first, string second, string third){
-        return null;
+    public List<string> CreateTeam(string first, string second, string third) {
+        return new List<string> { first, second, third };
     }
 }
